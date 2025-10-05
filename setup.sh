aad003c39a1780c5ac7d9555cd10034054eb0bad
Shauwki <halals@me.com>
2025-10-06 00:50:47 +0200

    added services

--- setup.sh
+++ setup.sh
@@ -3,15 +3,20 @@
 set -e
 
 echo "--- Plongo Setup Script ---"
-echo "[1/x] Benodigde mappen aanmaken..."
+echo "[1/3] Benodigde mappen aanmaken..."
 mkdir -p ./n8n/n8n_config
 mkdir -p ./n8n/postgres_data
 mkdir -p ./web/html
+mkdir -p ./obsidian/notes
+mkdir -p ./mqtt/config
 
-echo "[2/x] Correcte permissies instellen voor n8n..."
-sudo chown -R 1000:1000 ./n8n
+echo "[2/3] Correcte permissies instellen..."
+sudo chown -R 1000:1000 ./n8n/
+sudo chown -R 1000:1000 ./obsidian/
+sudo chown -R 1000:1000 ./nextcloud/
+sudo chown -R 1000:1000 ./mqtt/
 
-echo "[3/x] Controleren op .env bestand..."
+echo "[3/3] Controleren op .env bestand..."
 if [ ! -f ./.env ]; then
     echo "-> .env bestand niet gevonden. Aanmaken vanuit .env.example..."
     cp .env.example .env
